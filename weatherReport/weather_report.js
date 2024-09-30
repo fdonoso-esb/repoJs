90af47f56d00e68f2ae6c23c6155f5bfe9235871
fdonoso-esb <fdonoso@sb.cl>
2024-09-30 00:19:33 -0400

    weatherReport

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,9 +1,14 @@
 function showweatherDetails(event) {
       event.preventDefault();
 
- const city = document.getElementById('city').value;
+// const city = document.getElementById('city').value;
+//      const apiKey = 'ae7edf3d0c0dcfb6d58ca34b60e2b5b7'; // Replace 'YOUR_API_KEY' with your actual API key
+//      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+ const lat = document.getElementById('lat').value;
+ const lon = document.getElementById('lon').value;
       const apiKey = 'ae7edf3d0c0dcfb6d58ca34b60e2b5b7'; // Replace 'YOUR_API_KEY' with your actual API key
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 
  fetch(apiUrl)
